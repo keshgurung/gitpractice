447df011970866b3f1cb6a9bc350cab64c4c0b80
keshgurung <k_grg@hotmail.com>
2022-10-25 12:22:03 +0100

    added a mins func

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ function testing(a ,b){
   return a+b
 }
 
+function min(a ,b){
+  return a-b
+}
+
 console.log('here')
 
 console.log(testing(1,2))
@@ -14,3 +18,4 @@ console.log(testing(4,2))
 
 console.log(testing(4,4))
 
+console.log(min(9,4))
